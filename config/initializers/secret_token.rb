c6a5c4b3f51d5b004d6cafe366c2714bd1c2e339
nomatteus <matt@matthewruten.com>
2011-11-28 14:27:10 -0500

    Add basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinesafe::Application.config.secret_token = '2c30da2a585ec4e287871d2c63e79298a8231e79aaa6428577e93767fab5e395ad7b20f6e0430dd034e5818e6fb6f85393dfdb9f506cf87e00ab1136f18ed83a'
