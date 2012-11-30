d199d4ab7be3d4195f0ac1649104d1c45cfc97ba
nomatteus <matt@ruten.ca>
2012-11-30 03:05:20 -0500

    Specify JSON for infractions manually, and update text returned for "severity" field on infractions in API

--- app/models/infraction.rb
+++ app/models/infraction.rb
@@ -1,3 +1,16 @@
 class Infraction < ActiveRecord::Base
   belongs_to :inspection
+
+  def severity_for_api
+    case severity
+    when "S - Significant"
+      "Significant"
+    when "M - Minor"
+      "Minor"
+    when "C - Crucial"
+      "Crucial"
+    else
+      self.severity
+    end
+  end
 end
