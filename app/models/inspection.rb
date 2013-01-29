262aaba7af015db4acb4fb2f3bbd95fa0d7a8a6f
nomatteus <matt@ruten.ca>
2013-01-29 07:05:52 -0500

    Fix issue where infractions sometimes didn't have a severity
    And TODO for later: Figure out why there are a few like this. For now, just rejecting them.

--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -11,8 +11,10 @@ class Inspection < ActiveRecord::Base
     # If not found in SORT_ORDER, then sort by the ascii code of character
     #   "A".ord is 65, so these will all appear in alphabetical order after 
     #    anything in SORT_ORDER
-    self.infractions.sort_by { |infraction| 
-      SORT_ORDER.index(infraction.severity[0].upcase) || infraction.severity[0].upcase.ord
-    }
+    self.infractions
+      .reject{ |infraction| infraction.severity.empty? }
+      .sort_by { |infraction| 
+        SORT_ORDER.index(infraction.severity[0].upcase) || infraction.severity[0].upcase.ord
+      }
   end
 end
