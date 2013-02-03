1e82b4c2ec79a91ed323f9c80ce694f6e3be13e9
nomatteus <matt@ruten.ca>
2013-02-03 01:06:37 -0500

    Fix tests to get everything up-to-date and passing again, and add jbuilder gem

--- test/functional/establishments_controller_test.rb
+++ test/functional/establishments_controller_test.rb
@@ -14,18 +14,85 @@ class EstablishmentsControllerTest < ActionController::TestCase
       "latest_name" => est1.latest_name.titleize,
       "latest_type" => est1.latest_type,
       "address" => est1.address.titleize,
-      "latlng" => est1.latlng,
+      "latlng" => {"lat" => 43.6982039, "lng" => -79.5027124},
+      "inspections" => [
+          "id" => 1,
+          "status" => "Pass",
+          "date" => "2011-11-30",
+          "minimum_inspections_per_year" => 1,
+          "establishment_name" => "Establishment Name",
+          "establishment_type" => "Restaurant"
+        ]
       }, {
       "id" => est2.id,
       "latest_name" => est2.latest_name.titleize,
       "latest_type" => est2.latest_type,
       "address" => est2.address.titleize,
-      "latlng" => est2.latlng,
+      "latlng" => {"lat" => 43.688205, "lng" => -79.5027122},
+      "inspections" => [
+          "id" => 2,
+          "status" => "Conditional Pass",
+          "date" => "2011-11-29",
+          "minimum_inspections_per_year" => 3,
+          "establishment_name" => "Establishment Name 2",
+          "establishment_type" => "Food Take Out"
+        ]
       }
       ],
       "paging" => {
         "current_page" => 1,
         "total_pages" => 1
+      }}, parsed_body)
+  end
+
+  test "should get show json" do
+    est2 = establishments(:two)
+    get :show, :format => :json, :id => est2.id
+    parsed_body = JSON.parse(response.body)
+    assert_response :success
+    assert_equal({"data" => {
+      "id" => est2.id,
+      "latest_name" => est2.latest_name.titleize,
+      "latest_type" => est2.latest_type,
+      "address" => est2.address.titleize,
+      "latlng" => {"lat" => 43.688205, "lng" => -79.5027122},
+      "inspections" => [
+          "id" => 2,
+          "status" => "Conditional Pass",
+          "date" => "2011-11-29",
+          "infractions" => [
+            {
+              "id" => 980190962,
+              "inspection_id" => 2,
+              "details" => "Operator fail to properly remove liquid waste",
+              "severity" => "Crucial",
+              "action" => "Summons",
+              "court_outcome" => "Guilty",
+              "amount_fined" => 100
+            },
+            {
+              "id" => 298486374,
+              "inspection_id" => 2,
+              "details" => "Details for two",
+              "severity" => "Significant",
+              "action" => "Summons",
+              "court_outcome" => "Stayed",
+              "amount_fined" => 0
+            },
+            {
+              "id" => 113629430,
+              "inspection_id" => 2,
+              "details" => "Details for three",
+              "severity" => "Minor",
+              "action" => "Something",
+              "court_outcome" => "An Outcome",
+              "amount_fined" => 1000
+            }
+          ],
+          "minimum_inspections_per_year" => 3,
+          "establishment_name" => "Establishment Name 2",
+          "establishment_type" => "Food Take Out"
+        ],
       }}, parsed_body)
   end
 
