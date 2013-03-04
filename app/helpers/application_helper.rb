133a9dcab64d79152fb6751f9fab0b650593f9e9
nomatteus <matt@ruten.ca>
2013-03-03 23:24:01 -0500

    Add event tracking in addition to conversion tracking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,7 +38,9 @@ module ApplicationHelper
   end
 
   def onclick_conversion_track
-    "trackConversion(1071773858, '5_HnCMu2pQQQovGH_wM'); return true;".html_safe
+    conversion_track ="" # "trackConversion(1071773858, '5_HnCMu2pQQQovGH_wM')"
+    event_track = "_gaq.push(['_trackEvent', 'App Store Link', 'Click'])"
+    "#{conversion_track};#{event_track};return true;".html_safe
   end
 
 end
